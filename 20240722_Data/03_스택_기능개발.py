bd3921ec21baf006fe2e36b83229fd4c8eeb8969
gunhee.kim <gunhee.kim@halla.com>
2024-07-29 11:55:51 +0900

    20240722 Data Structure : Hash Stack Heap

--- 20240722_Data/03_스택_기능개발.py
+++ 20240722_Data/03_스택_기능개발.py
@@ -1,6 +1,6 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42586
 
-def solution(progresses, speeds):
+def solution_(progresses, speeds):
     length = len(progresses)
     success = 0
     answer = []
@@ -23,4 +23,24 @@ def solution(progresses, speeds):
             answer.append(cnt)
             success += cnt
 
+    return answer
+
+def solution(progresses:list, speeds:list):
+    answer = list()
+    time = 0
+    success = 0
+    
+    while progresses:
+        if (progresses[0] + time*speeds[0]) >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            success += 1
+        else:
+            if success > 0:
+                answer.append(success)
+                success = 0
+            time += 1
+    
+    answer.append(success)
+    
     return answer
